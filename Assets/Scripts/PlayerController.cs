11a66682a768a0113a5c5f198520799b44a707eb
martinwthalex <alemamartin01@gmail.com>
2023-10-26 02:20:43 +0200

    Salto
    Falta que dar con los numeros exactos para que quede bien

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
-     int vel = 18, jumpForce = 18;
+    float vel = 15, jumpForce = 20;
     bool onFloor = false;
+    public LayerMask mascara;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +18,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Movement(vel);
-        
         Jump();
+        DetectFloor();
         //print(onFloor);
     }
 
-    void Movement(int vel)
+    void Movement(float vel)
     {
         rb.velocity = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.RightArrow))
@@ -33,18 +34,42 @@ public class PlayerController : MonoBehaviour
     }
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && onFloor == true)
+        if (Input.GetKey(KeyCode.UpArrow) && onFloor)
         {
-            print("salto");
-            //rb.AddForce(Vector2.up * jumpForce);
-            rb.velocity += new Vector2(0, jumpForce);
+            //print("salto");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            
         }
 
     }
+    void DetectFloor()
+    {
+        // Hay que crear un origen, una direccion y una distancia
+        Vector3 origen = transform.position;
+        Vector3 direccion = Vector3.down;
+        float distancia = 0.2f;
 
+        RaycastHit2D hit = Physics2D.Raycast(origen, direccion, distancia,mascara); // mascara es donde solo se va a fijar, en unity se pueden seleccionar las capas 
+                                                                                     // ignorará todos los colliders que estén fuera de la capa que le indiquemos
+
+
+
+
+        Debug.DrawRay(origen, direccion * distancia, Color.red);
+        onFloor = false;
+        if (hit)// si esa variable de tipo raycast tiene información , es decir si ha colisionado con algo
+        {
+           // Debug.Log("tag: "+ hit.collider.tag);
+            if (hit.collider.tag == "Suelo") // el tag del objeto con el que ha colisionado
+            {
+                onFloor = true;
+            }
+        }
+
+    }
     //private bool OnFloor()
     //{
-        
+
     //    Vector3 origen = transform.position;
     //    Vector3 direccion = Vector3.down;
     //    float distancia = 0.2f;
@@ -61,22 +86,22 @@ public class PlayerController : MonoBehaviour
     //            onFloor = true;
     //            //print("floor");
     //        }
-                
+
     //    }
     //    else onFloor = false;
     //    return onFloor;
     //}
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Floor"))
-        {
-            print("suelo");
-            //Digo que no está saltando (para que pueda volver a saltar)
-            onFloor = true;
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Floor"))
+    //    {
+    //        print("suelo");
+    //        //Digo que no está saltando (para que pueda volver a saltar)
+    //        onFloor = true;
             
-            //Le quito la fuerza de salto remanente que tuviera
-            rb.velocity = new Vector2(rb.velocity.x, 0);
-        }
-        else onFloor = false;
-    }
+    //        //Le quito la fuerza de salto remanente que tuviera
+    //        rb.velocity = new Vector2(rb.velocity.x, 0);
+    //    }
+    //    else onFloor = false;
+    //}
 }
