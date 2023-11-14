922f0a9f23488f7da6668f780ec345a8016eaf17
informaticaesne <informatica@esne.es>
2023-11-14 13:48:21 +0100

    El menu esta bug x culpa del script de agrandar texto

--- Assets/Scripts/MenuInicial/MenuInicial2.cs
+++ Assets/Scripts/MenuInicial/MenuInicial2.cs
@@ -3,28 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuIncial : MonoBehaviour
+public class MenuInicial2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void Jugar()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    
     public void Salir()
     {
-        Debug.Log("SALIR...");
+        
         Application.Quit();
     }
 }
